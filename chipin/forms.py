f682b050fff7e9b9f9ccb020f8696222f3bf60ac
Suggley <ayden@cavtech.com.au>
2025-03-05 12:47:07 +1100

    Chat Creation

--- chipin/forms.py
+++ chipin/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Group
+from .models import Comment
 
 class GroupCreationForm(forms.ModelForm):
     class Meta:
@@ -10,6 +11,7 @@ class GroupCreationForm(forms.ModelForm):
         self.user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
 
+    
     def save(self, commit=True):
         group = super().save(commit=False)
         group.admin = self.user  # Assign the logged-in user as the admin
@@ -17,3 +19,19 @@ class GroupCreationForm(forms.ModelForm):
             group.save()
             group.members.add(self.user)  # Add the admin to the members list
         return group
+    
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Enter your comment...'})
+        }
+
+    # Clean the content to sanitise input
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if "<script>" in content.lower():  # Prevent XSS by checking for script tags
+            raise forms.ValidationError("Invalid content.")
+        return content
